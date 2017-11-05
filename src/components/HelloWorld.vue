9d034d3bab4aa52e92b7a2a23792e3f4b28f8333
OrangeAtHome <fatorange910213@hotmail.com>
2017-11-05 22:44:22 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <button type="button" class="btn btn-success" @click="onPaymentClick">订单确认</button>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+      onPaymentClick() {
+        this.$ajax({
+          method: 'post',
+          dataType: 'JSONP',
+          url: 'http://localhost:49851/Api/Payment/GetPaymentChange',
+        }).then(a => {
+          console.log(a)
+        })
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2{
+    font-weight:normal;
+  }
+  ul{
+    list-style-type:none;
+    padding:0;
+  }
+  li{
+    display:inline-block;
+    margin:0 10px;
+  }
+  a{
+    color:#42b983;
+  }
+</style>
